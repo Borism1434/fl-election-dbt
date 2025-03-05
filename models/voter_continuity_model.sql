868c256aa5a95335d4c5ec5212402db63e75f030
Borism1434 <150632878+Borism1434@users.noreply.github.com>
2025-03-05 13:55:28 -0500

    correction: account for the multiple instances in voter_history_code in the voter history tables

--- models/voter_continuity_model.sql
+++ models/voter_continuity_model.sql
@@ -22,7 +22,9 @@ voter_history as (
     select 
         voter_id, 
         COALESCE(NULLIF(vote_history_code, ''), 'NV') as vote_history_code,
-        2016 as election_year
+        2016 as election_year,
+        vote_date as election_date,
+        election_code
     from {{ source('voterfile', 'election_history_2016') }}
 
     union all
@@ -30,7 +32,9 @@ voter_history as (
     select 
         voter_id, 
         COALESCE(NULLIF(vote_history_code, ''), 'NV') as vote_history_code,
-        2018 as election_year
+        2018 as election_year,
+        vote_date as election_date,
+        election_code
     from {{ source('voterfile', 'election_history_2018') }}
 
     union all
@@ -38,7 +42,9 @@ voter_history as (
     select 
         voter_id, 
         COALESCE(NULLIF(vote_history_code, ''), 'NV') as vote_history_code,
-        2020 as election_year
+        2020 as election_year,
+        vote_date as election_date,
+        election_code
     from {{ source('voterfile', 'election_history_2020') }}
 
     union all
@@ -46,7 +52,9 @@ voter_history as (
     select 
         voter_id, 
         COALESCE(NULLIF(vote_history_code, ''), 'NV') as vote_history_code,
-        2022 as election_year
+        2022 as election_year,
+        vote_date as election_date,
+        election_code
     from {{ source('voterfile', 'election_history_2022') }}
 
     union all
@@ -54,7 +62,9 @@ voter_history as (
     select 
         voter_id, 
         COALESCE(NULLIF(vote_history_code, ''), 'NV') as vote_history_code,
-        2024 as election_year
+        2024 as election_year,
+        election_date,
+        election_code
     from {{ source('voterfile', 'election_history_2024') }}
 ),
 
@@ -63,10 +73,10 @@ voter_continuity as (
         vd.voter_id,
 
         -- Get the earliest registration date
-        MIN(reg_date) as reg_date,
+        min(reg_date) as reg_date,
 
         -- Get the first and last year a voter appears
-        MIN(vh.election_year) as first_year,
+        min(vh.election_year) as first_year,
         max(vh.election_year) as last_year,
 
         -- Aggregate party and county information per voter
@@ -82,11 +92,11 @@ voter_continuity as (
         max(case when vh.election_year = 2022 then county end) as county_2022,
         max(case when vh.election_year = 2024 then county end) as county_2024,
         -- Aggregate vote method per election year
-        max(case when vh.election_year = 2016 then vh.vote_history_code end) as vote_history_code_2016,
-        max(case when vh.election_year = 2018 then vh.vote_history_code end) as vote_history_code_2018,
-        max(case when vh.election_year = 2020 then vh.vote_history_code end) as vote_history_code_2020,
-        max(case when vh.election_year = 2022 then vh.vote_history_code end) as vote_history_code_2022,
-        max(case when vh.election_year = 2024 then vh.vote_history_code end) as vote_history_code_2024
+        max(case when vh.election_year = 2016 and vh.election_code = '10282' and extract(year from vh.election_date) = 2016 then vh.vote_history_code end) as vote_history_code_2016,
+        max(case when vh.election_year = 2018 and vh.election_code = '10481' and extract(year from vh.election_date) = 2018  then vh.vote_history_code end) as vote_history_code_2018,
+        max(case when vh.election_year = 2020 and vh.election_code = '10866' and extract(year from vh.election_date) = 2020  then vh.vote_history_code end) as vote_history_code_2020,
+        max(case when vh.election_year = 2022 and vh.election_code = '26906' and extract(year from vh.election_date) = 2022  then vh.vote_history_code end) as vote_history_code_2022,
+        max(case when vh.election_year = 2024 and vh.election_code = 'GEN' and extract(year from vh.election_date) = 2024 then vh.vote_history_code end) as vote_history_code_2024
 
     from voter_details vd
     left join voter_history vh 
